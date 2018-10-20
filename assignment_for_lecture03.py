e086c02b8892e8062debe3fcd3639d72962003ba
Unknown <unnatbharat1011@gmail.com>
2018-10-20 18:56:21 +0530

    this is file by me ("Bharat1947") on assignment on lecture 03

---
+++ assignment_for_lecture03.py
@@ -0,0 +1,47 @@
+import time
+# introduction to python
+#question 01
+print("Answer to question 01")
+print("Something")
+time.sleep(1)
+#question 02
+print("Answer to question 02")
+a="good"
+b = "strings"
+c=a+b
+print(c)
+time.sleep(1)
+#question 03
+print("Answer to question 03")
+a=int(input("your desired holidays"))
+b=int(input("Your desired maths marks"))
+c=int(input("your desired bank balance"))
+print("holidays")
+print(a)
+print("maths marks")
+print(b)
+print("bank balance")
+print(c)
+time.sleep(1)
+#question 04
+print("Answer to question 04")
+name = 'bharat'
+number = 24
+zodiac = 'python'
+print('%s %d %s' %(name,number,zodiac))
+time.sleep(1)
+#question 05
+import time
+print("Answer to question 05")
+pi = 3.14
+r= int(input("radius of the circle"))
+area = pi*r*r
+print('area of circle is :')
+print(area)
+time.sleep(2)
+#question
+i = 100
+#get a starry screen
+'''for i in range(0,100):
+    print("*"*100,end="")
+print("** ")'''
