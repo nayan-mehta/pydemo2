c65635987c84d7c1bafec6ba0d25d238e6544593
Akash <aaku17.10.1998@gmail.com>
2018-10-21 19:15:39 +0530

    1

---
+++ prac.py
@@ -0,0 +1,19 @@
+#first assignment
+print("hello my name is akash ")
+a= ("äcad")
+b= ("view")
+print(a+b)
+name= str(input("what is your name?"))
+print(name)
+age= int(input("what is your age?"))
+print(age)
+print("let's get started",end=".\n")
+s= ("acadview")
+course= ("python")
+fee= (5000)
+print('%s %s %d '%(s,course,fee))
+pi= (3.14)
+radius= int(input("what is the radius of circle "))
+answer= pi*radius*radius
+print("area of circle is ", answer)
+
