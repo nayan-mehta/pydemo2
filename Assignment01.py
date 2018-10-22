6f603190c8909743a5956b7fb1e88c735d2e3d39
Unknown <unnatbharat1011@gmail.com>
2018-10-22 20:27:14 +0530

    Assignment 01 : This assignment is based on topic : --  Introduction To Python.

---
+++ Assignment01.py
@@ -0,0 +1,40 @@
+#assignment01
+#Q.1- Print anything you want on screen.
+#Answer
+print("Acad_View")
+#Q.2- Join two strings using '+'. E.g.-"Acad"+"View”
+#Answer
+a="acad"
+b="view"
+print(a+b)
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+#Answer
+x=int(input("x"))
+y=int(input("y"))
+z=int(input("z"))
+print("Three Variables: ")
+print(x)
+print(y)
+print(z)
+#Q.4- Print “Let’s get started” on screen.
+#Answer
+print("Let’s get started")
+#Q.5- Print the following values using placeholders. 
+#Answer
+s="Acadview" 
+course="Python" 
+fees=5000
+print(s)
+print(course)
+print(fees)
+#Q.6- Find the area of circle 
+#pi = 3.14 
+#Take radius as input from user 
+#Print the area of circle
+#Answer
+r=int(input("radius"))
+pi= 3.14
+area=pi*r*r
+print(area)
+print("square-unit")
+#end.
